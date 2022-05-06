cab7157f1241aa21db0a3c067fb5ae10e1eaa81d
khandei92 <104444685+khandei92@users.noreply.github.com>
2022-05-06 13:25:10 +0530

    Update v_wiptask.sql

---
+++ models/example/v_wiptask.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+select  wip.TASKID,
+        wip.TASKNAME,
+        wip.TICKETNAME,
+        wip.ORDERID,
+        wip.AELOCATIONNAME
+From "PERFORM"."STG"."WIPTASKS" wip
+Left outer join "PERFORM"."STG"."TASKMSGS" tak
+on wip.TASKID = tak.TASKID
+Where wip.TICKETNAME = 'TRP_AutoTrap'
